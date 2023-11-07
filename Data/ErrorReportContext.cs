dc8754a18a6224c32255ba56c3de38428cf33f5f
adessolv <adesso.sevici@gmail.com>
2023-11-07 14:20:06 +0100

    nrdf

--- Data/ErrorReportContext.cs
+++ Data/ErrorReportContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ErrorReport.Model;
 
 namespace ErrorReport.Data
@@ -14,6 +10,6 @@ namespace ErrorReport.Data
         {
         }
 
-        public DbSet<ErrorReport.Model.Report> Report { get; set; } = default!;
+        public DbSet<Report> Report { get; set; } = default!;
     }
 }
